6270710598117ea88649b2f396f431cdbe5b489a
Pawel Labno <pawel.labno@outlook.com>
2018-04-03 21:40:45 +0200

    log gamestate - update

--- src/Helpers/StatePrint.py
+++ src/Helpers/StatePrint.py
@@ -1,4 +1,4 @@
-import sys
+import statistics
 
 from src.Enums.Colors import Colors
 
@@ -19,7 +19,44 @@ class StatePrint:
         output.append(player.Wagons)
         output.append(player.Points)
 
-        
+        output.append(len(game.players))
+
+        points = []
+        wagons = []
+        minWagon = float("inf")
+        maxWagon = 0
+        minPoints = float("inf")
+        maxPoints = 0
+        sumPoints = 0
+        sumWagons = 0
+        for player in game.players:
+            if player.Points > maxPoints:
+                maxPoints = player.Points
+            if player.Points < minPoints:
+                minPoints = player.Points
+            points.append(player.Points)
+            sumPoints += player.Points
+            if player.Wagons > maxWagon:
+                maxWagon = player.Wagons
+            if player.Wagons < minWagon:
+                minWagon = player.Wagons
+            wagons.append(player.Wagons)
+            sumWagons += player.Wagons
+
+        medianPoints = statistics.median(points)
+        avgPoints = sumPoints / len(points)
+        medianWagons = statistics.median(wagons)
+        avgWagons = sumWagons / len(wagons)
+
+        output.append(minPoints)
+        output.append(maxPoints)
+        output.append(avgPoints)
+        output.append(medianPoints)
+
+        output.append(minWagon)
+        output.append(maxWagon)
+        output.append(avgWagons)
+        output.append(medianWagons)
 
         print(output)
         return output
